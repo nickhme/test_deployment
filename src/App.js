19fb30d46423dcabec0fc65eb049de1a9b9484f5
Nick Hayes <nhayes559@gmail.com>
2020-10-25 14:48:09 +0000

    test deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './styles/style.scss'
 
 const App = () => (
   <BrowserRouter>
+    <div>Hello nick</div>
     <Switch>
       <Route exact path="/test_deployment/nick" component={Home}/>
       <Route exact path="/test_deployment" component={Cheeses}/>
