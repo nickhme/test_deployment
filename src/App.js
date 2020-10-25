6e2478da0424528c23fad2babfa4071c740fab3d
Nick Hayes <nhayes559@gmail.com>
2020-10-25 14:39:19 +0000

    test deploy

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import 'bulma'
+import './styles/style.scss'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/nick" component={Home}/>
+      <Route exact path="/" component={Cheeses}/>
+    </Switch>
+  </BrowserRouter>
+)
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const Cheeses = () => {
+  return <h1>Cheese</h1>
+}
+
+export default App
