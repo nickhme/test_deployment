56a2fc96c1b97ca779ea1defef58e8a58ba17b20
Nick Hayes <nhayes559@gmail.com>
2020-10-25 16:02:38 +0000

    test deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,41 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve('.'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.(png|jpe?g|gif)$/i, use: 'file-loader' }
-    ]
-  },
-  devServer: {
+module.exports = env => {
+  const publicPath = env.NODE_ENV !== 'local' ? {
     publicPath: '/',
-    contentBase: path.resolve('src'),
-    hot: true,
-    open: true,
-    port: 8000,
-    watchContentBase: true,
-    historyApiFallback: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ]
+  } : {}
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve('.'),
+      ...publicPath
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+        { test: /\.(png|jpe?g|gif)$/i, use: 'file-loader' }
+      ]
+    },
+    devServer: {
+      publicPath: '/',
+      contentBase: path.resolve('src'),
+      hot: true,
+      open: true,
+      port: 8000,
+      watchContentBase: true,
+      historyApiFallback: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      })
+    ]
+  }
 }
